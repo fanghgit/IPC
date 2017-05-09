51a2cf31b7b5032fa147e1b52770ddfa3b8017f1
Huang Fang <fangh@hilbert.ucdavis.edu>
2017-05-09 15:57:55 -0700

    add data script

---
+++ dimReduce/matlab/testdata/getdata.sh
@@ -0,0 +1,6 @@
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/news20.binary.bz2
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_train.binary.bz2
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_test.binary.bz2
+
+bzip2 -d *.bz2
+
