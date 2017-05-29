2853cebc108c5ee6fe0eff914665b311fed18466
Huang Fang <fangh@hilbert.ucdavis.edu>
2017-05-28 23:28:02 -0700

    change testSSNMF

--- dimReduce/matlab/testSSNMF.m
+++ dimReduce/matlab/testSSNMF.m
@@ -1,8 +1,13 @@
 [tl,tf] = libsvmread('testdata/news20/0.1/trtr');
 [vl,vf] = libsvmread('testdata/news20/0.1/vtr');
+tf( ~any(tf,2), : ) = [];  %rows
+vf( ~any(vf,2), : ) = [];  %rows
+
 maxd = max(size(tf,2),size(vf,2));
 vf(size(vf,1),maxd)=0;
 X = [tf' vf'];
+X( ~any(X,2), : ) = [];
+
 Y = zeros(2,size(tl,1));
 for i=1:size(tl)
 	if tl(i)==-1
@@ -11,6 +16,8 @@ for i=1:size(tl)
 		Y(2,i)=1;
 	end
 end
+
+
 tridx = [1:size(tl,1)];
 teidx = [size(tl,1)+1:size(tl,1)+size(vl,1)];
 [B,S] = SSNMF(X,Y,10,1, tridx , teidx, 1e-5, 10);
